ad0d49992a8aaf7efa28ae09729c0666771135dc
nagelflorian <florian.nagel@yahoo.de>
2015-07-04 02:00:59 +0200

    Added Instagram Hashtags

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# WORK IN PROGRESS
-
 [![Build Status](https://travis-ci.org/nagelflorian/meteor-social-media-feed.svg)](https://travis-ci.org/nagelflorian/meteor-social-media-feed)
 # Meteor - Social Media Feed
 
 ![Social media feed running with Masonry](https://cloud.githubusercontent.com/assets/7649376/8296111/afbc67d0-194c-11e5-8685-5d4ea9e0ebf2.png)
 
-A social media feed for [Meteor](http://meteor.com). The package aggregates content from all connected social media profiles and builds up a mosaic from the gathered content. You can manually invoke a content update or schedule an update using something like `[percolate:synced-cron](https://github.com/percolatestudio/meteor-synced-cron)`.
-
-This package doesn't come with a grid layout library like [Masonry](http://masonry.desandro.com) in order for you to decide on which one you'd like to use.
+A social media feed for [Meteor](http://meteor.com). It aggregates content from all connected social media profiles and builds up a mosaic from the gathered content. This package doesn't come with a grid layout library like [Masonry](http://masonry.desandro.com) in order for you to decide on which one you'd like to use.
 
 **Please follow the API Terms of Use of each connected social network.**
 
@@ -25,7 +21,8 @@ Create a 'setting.json' file at the root of your application with your api-keys 
 {
 	"instagram_keys": {
 		"client_id": "<CLIENTID>",
-		"client_secret": "<CLIENTSECRET>"
+		"client_secret": "<CLIENTSECRET>",
+		"access_token": "<ACCESSTOKEN>"
 	},
 	"facebook_keys": {
 		"app_id": "<APPID>",
@@ -35,19 +32,68 @@ Create a 'setting.json' file at the root of your application with your api-keys 
 }
 ```
 
-Next add your Ids for each social network profile using the `config` method on the server:
+Next add profile Ids or hashtags using the `config` method on the server:
 
 ```js
 SocialMediaFeed.config({
+	facebookIds: ["123456789010"],
 	instagramIds: ["12345678"],
-	facebookIds: ["123456789010"]
+	instagramTags: ["tbt"]
 });
 ```
 
 Include the template somewhere on the client:
 
 ```js
-  {{> SocialMediaFeed}}
+{{> SocialMediaFeed}}
 ```
 
 Call `SocialMediaFeed.update();` on the server and run your app with `meteor --settings=settings.json` and the stream should appear within seconds.
+
+## API
+
+You can only access the API on the server. Wrap the functions in `Meteor.methods({...});` if you want them to be available on the client.
+
+### Options
+
+You can configure **Social Media Feed** using the following options:
+
+```js
+SocialMediaFeed.config({
+	// log details to console
+	log: false,
+	// enable pagination of data
+	pagination: false,
+
+	facebookIds: [],
+	instagramIds: [],
+	instagramTags: [],
+	twitterIds: [],
+});
+```
+
+### Update
+
+To gather content from the connected networks call:
+
+```js
+SocialMediaFeed.update();
+```
+
+To schedule automatic updates you can use something like `[percolate:synced-cron](https://github.com/percolatestudio/meteor-synced-cron)`.
+
+### Reset
+
+To reset your feed and remove all data from the collection call:
+
+```js
+SocialMediaFeed.reset();
+```
+
+### Hide Content
+
+You can hide content from the stream by calling:
+
+```js
+SocialMediaFeed.hideContent(network, contentId);
+```
--- social-media-feed-tests.js
+++ social-media-feed-tests.js
@@ -5,48 +5,105 @@ if (Meteor.isServer) {
 		FeedCollection.remove({});
 		test.equal(FeedCollection.find().count(), 0);
 	});
+
 	Tinytest.add('SocialMediaFeed Options are default values and profile ids are empty', function (test) {
 		test.equal(SocialMediaFeed.options.log, false);
 		test.equal(SocialMediaFeed.options.pagination, false);
 		test.equal(SocialMediaFeed.options.facebookIds, []);
 		test.equal(SocialMediaFeed.options.instagramIds, []);
+		test.equal(SocialMediaFeed.options.instagramTags, []);
 	});
+
 	Tinytest.add('Default SocialMediaFeed (without profile ids) update adds nothing to collection', function (test) {
 		SocialMediaFeed.update();
 		test.equal(FeedCollection.find().count(), 0);
 	});
+
 	Tinytest.add('User can add profile ids', function (test) {
 		SocialMediaFeed.config({
 			facebookIds: "7919071058",
-			instagramIds: ["25025320", "20120087", "224223453", "365041587"]
+			instagramIds: ["25025320", "20120087", "224223453", "365041587"],
+			instagramTags: ["tbt", "meteorjs"]
 		});
 		test.equal(SocialMediaFeed.options.facebookIds, "7919071058");
 		test.equal(SocialMediaFeed.options.instagramIds[1], "20120087");
+		
 		// reset config
 		SocialMediaFeed.config({
 			facebookIds: [],
 			instagramIds: []
 		});
 	});
-	Tinytest.addAsync('Update SocialMediaFeed gets results and adds them to collection', function (test, onComplete) {
+
+	Tinytest.addAsync('Get Facebook Data', function (test, onComplete) {
 		SocialMediaFeed.config({
-			facebookIds: "7919071058",
-			instagramIds: ["25025320", "20120087", "224223453", "365041587"]
+			facebookIds: ["7919071058"]
 		});
 
 		SocialMediaFeed.update();
 
 		Meteor.setTimeout(function() {
-			test.notEqual(FeedCollection.find().count(), 0);
+			test.notEqual(FeedCollection.find({social_network: "facebook"}).count(), 0);
+
+			// reset config + collection
+			SocialMediaFeed.config({facebookIds: []});
+			SocialMediaFeed.reset();
+			test.equal(FeedCollection.find({social_network: "facebook"}).count(), 0);
+
 			onComplete();
 		}, 5000);
-
-		// reset config
-		SocialMediaFeed.config({
-			facebookIds: [],
-			instagramIds: []
-		});
 	});
+
+	Tinytest.addAsync('Get Instagram Profile Data', function (test, onComplete) {
+		SocialMediaFeed.config({
+			instagramIds: ["25025320"]
+		});
+
+		SocialMediaFeed.update();
+
+		Meteor.setTimeout(function() {
+			test.notEqual(FeedCollection.find({social_network: "instagram"}).count(), 0);
+
+			// reset config + collection
+			SocialMediaFeed.config({instagramIds: []});
+			SocialMediaFeed.reset();
+			test.equal(FeedCollection.find({social_network: "instagram"}).count(), 0);
+
+			onComplete();
+		}, 5000);
+	});
+
+	Tinytest.addAsync('Get Instagram Hashtag Data', function (test, onComplete) {
+		SocialMediaFeed.config({
+			instagramTags: ["tbt"]
+		});
+
+		SocialMediaFeed.update();
+
+		Meteor.setTimeout(function() {
+			test.notEqual(FeedCollection.find({social_network: "instagram"}).count(), 0);
+
+			// reset config + collection
+			SocialMediaFeed.config({instagramIds: []});
+			SocialMediaFeed.reset();
+			test.equal(FeedCollection.find({social_network: "instagram"}).count(), 0);
+
+			onComplete();
+		}, 5000);
+	});
+
+	Tinytest.add('Hide Content From Stream', function (test) {
+		// insert dummy content
+		FeedCollection.insert({"social_network": "instagram", "content_id": "1234"})
+
+		SocialMediaFeed.hideContent("instagram", "1234");
+		test.equal(1, 1);
+
+		// remove content from collection
+		SocialMediaFeed.reset();
+		test.equal(FeedCollection.find().count(), 0);
+	});
+
 	Tinytest.add('Reset SocialMediaFeed returns an empty SocialMediaFeed', function (test) {
 		SocialMediaFeed.reset();
 		test.equal(FeedCollection.find().count(), 0);
