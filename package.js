00daa68b9e5b3bcf8cb4aa1d018082cecd77f43f
nagelflorian <florian.nagel@yahoo.de>
2015-07-21 17:13:22 +0200

    Added Meteor Logging - Fixes #1

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
 	name: 'floriannagel:social-media-feed',
-	version: '0.2.0',
+	version: '0.1.0',
 	// Brief, one-line summary of the package.
-	summary: 'Aggregated social media feed',
+	summary: 'Aggregated social media feed for Meteor',
 	// URL to the Git repository containing the source code for this package.
 	git: 'https://github.com/nagelflorian/meteor-social-media-feed.git',
 	// By default, Meteor will default to using README.md for documentation.
@@ -12,10 +12,11 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.1.0.2');
+	api.use(['underscore', 'check', 'logging'], 'server');
 	api.use(['templating', 'jquery'], 'client');
 	api.use(['mongo', 'http', 'voodoohop:masonrify', 'momentjs:moment'], ['client', 'server']);
 	api.addFiles('lib/collection.js', ['client', 'server']);
-	api.addFiles(['lib/server/publications.js','lib/server/social-media-feed.js', 'lib/server/facebook-api.js', 'lib/server/instagram-api.js'], 'server');
+	api.addFiles(['lib/server/publications.js','lib/server/social-media-feed.js'], 'server');
 	api.addFiles(['lib/client/helpers.js', 'lib/client/grid.html', 'lib/client/grid.js', 'lib/client/style.css'], 'client');
 
 	api.export('SocialMediaFeed', 'server');
